23e01e0d458042c29332adc43931f9a01417a9a8
Juan García <juanlegar@gmail.com>
2018-04-16 04:13:21 +0200

    Finnish project.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,17 +1,28 @@
+var Trackster = {};
+const API_KEY = 'd888c634dcc94aca3fe3862eb8ad1cdb';
+
 $(document).ready(function(){
   $('.btn-search').click(function(){
     Trackster.searchTracksByTitle($('input[name=search_str]').val());
   });
 });
 
-var Trackster = {};
-
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-
+  $("#list-data").empty();
+  for (var i=0; i<tracks.length; i++){
+    var mediumAlbumArt = tracks[i].image[1]["#text"];
+    var itemStructure = '<div class="row">'+
+            '<div class="col-sm-4"> <a href="' + tracks[i].url + '" target="_blank"><span class="glyphicon glyphicon-play-circle"></span></a>' + tracks[i].name + '</div>'+
+            '<div class="col-sm-3">' + tracks[i].artist + '</div>'+
+            '<div class="col-sm-3"> <img src="' + mediumAlbumArt + '" alt=""> </div>'+
+            '<div class="col-sm-2">' + tracks[i].listeners.toLocaleString("latn") +'</div>'+
+          '</div>';
+    $("#list-data").append(itemStructure);
+  };
 };
 
 /*
@@ -19,5 +30,10 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track='+title+'&api_key='+API_KEY+'&format=json',
+    success: function(data){
+      Trackster.renderTracks(data.results.trackmatches.track);
+    }
+  });
 };
